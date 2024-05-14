1a16a4a2909cea9b1a627ab410fb0db9ff6603a6
MtzFranVictorManuel <zs22024137@estudiantes.uv.mx>
2024-05-13 19:29:55 -0600

    terminado

--- obj/Debug/net8.0/backEndpuntoNet.AssemblyInfo.cs
+++ obj/Debug/net8.0/backEndpuntoNet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backEndpuntoNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5432dbb7c8239c1a83a7c73f3f289cc4371413bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a8eaf7be24de2c9db350a3020d114a64f1db7cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("backEndpuntoNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backEndpuntoNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
